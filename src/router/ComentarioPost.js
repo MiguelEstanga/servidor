becb9d1715e31cee5efa1e836638bb37caa8eae0
MiguelEstanga <115328770+MiguelEstanga@users.noreply.github.com>
2022-10-11 00:23:24 -0400

    Update ComentarioPost.js

--- src/router/ComentarioPost.js
+++ src/router/ComentarioPost.js
@@ -1,13 +1,17 @@
 const Comentario = require('express').Router()
 const ComentarioModelo  = require("../models/ComentarioModel")
-
+const uploadsImagenes = require("../../util/cloudinary")
 Comentario.post("/Comentario" , async (req , res) => {
     const {Comentario , Nombre , Email} = req.body;
     try {
+        const { tempFilePath } = req.files.file
+        //achivando imagenes en lineas
+        const resultado = await uploadsImagenes(tempFilePath , "Avatars") 
         const data = new ComentarioModelo({
             Comentario,
             Nombre,
-            Email
+            Email,
+            Avatar:resultado.url
         })
 
         await data.save();
